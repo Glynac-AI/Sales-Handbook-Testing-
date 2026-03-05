2afb3f8639a17c3497dfb38047a0c2c73afc3a87
Glynac-AI <friendykaliman@gmail.com>
2026-03-06 06:52:44 +0700

    Fix strapi/src/index.ts: remove broken Strapi import

--- strapi/src/index.ts
+++ strapi/src/index.ts
@@ -1,11 +1,10 @@
-import { Strapi } from '@strapi/strapi';
-
 export default {
-    register(/*{ strapi }: { strapi: Strapi }*/) {
+    register(/*{ strapi }*/) {
         // Place your application's register phase code here.
     },
 
-    bootstrap(/*{ strapi }: { strapi: Strapi }*/) {
+    bootstrap(/*{ strapi }*/) {
         // Place your application's bootstrap phase code here.
     },
 };
+
