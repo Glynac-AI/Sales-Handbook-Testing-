de9fe85c2c6b3c550f5e334a9e2f62acc822d0be
Glynac-AI <friendykaliman@gmail.com>
2026-03-06 06:54:21 +0700

    Fix plugins.ts: use Strapi 5 kebab-case key 'users-permissions' not 'plugin::users-permissions'

--- strapi/config/plugins.ts
+++ strapi/config/plugins.ts
@@ -1,9 +1,10 @@
 export default ({ env }: { env: (key: string, defaultValue?: string) => string }) => ({
-    'plugin::users-permissions': {
+    'users-permissions': {
         enabled: true,
         config: {
             jwtSecret: env('JWT_SECRET'),
         },
     },
 });
+
 
